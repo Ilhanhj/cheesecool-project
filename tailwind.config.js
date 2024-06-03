d08371b063a4853e281332b14b0c91a26b94d53d
Ilhan Haffiyan Juldan <ilhanzuldan11@gmail.com>
2024-06-03 12:01:13 +0700

    fix navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,21 @@ export default {
         "node_modules/preline/dist/*.js",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                dark: "#0d0d0d",
+            },
+            fontFamily: {
+                poppins: "poppins",
+            },
+            fontSize: {
+                sm: ["14px", "20px"],
+                base: ["16px", "24px"],
+                lg: ["18px", "28px"],
+                xl: ["20px", "28px"],
+                "3xl": ["32px", "38px"],
+            },
+        },
     },
     plugins: [require("preline/plugin")],
 };
