2fc9ef6b4f278199311243a93e62476255a37178
Ilhan Haffiyan Juldan <ilhanzuldan11@gmail.com>
2024-06-04 21:46:40 +0700

    fix: navbar dan index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,31 +1,29 @@
 @extends('layouts.main')
 
-@section('container')
-    
 
-    <!-- ========== HERO ========== -->
-    <div class="relative overflow-hidden  before:absolute before:top-0 before:start-1/2 before:bg-squaredBg3  before:bg-no-repeat before:bg-center before:size-full before:-z-[1] before:transform before:-translate-x-1/2">
+
+<!-- ========== HERO ========== -->
+<div class="relative overflow-hidden  before:absolute before:top-0 before:start-1/2 before:bg-glowBottom before:bg-no-repeat before:bg-center before:size-full before:-z-[1] before:transform before:-translate-x-1/2">
+  @section('container')
         <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pt-20 lg:pb-40">
       
           <!-- Title -->
-          <div class="max-w-xl mx-auto mt-5 text-center">
-            <h1 class="block text-4xl font-bold text-gray-800 md:text-5xl lg:text-6xl dark:text-neutral-200">
-              Supercharged Preline Experience
+          <div class="max-w-5xl mx-auto mt-5 leading-8 text-center ">
+            <h1 class="block text-3xl font-bold text-white md:text-5xl lg:text-6xl ">
+              Selamat datang di Universitas <span class="font-bold text-center text-transparent from-purple-600 via-pink-600 to-blue-600 bg-gradient-to-r bg-clip-text">Terbaik di Indonesia</span>
             </h1>
           </div>
           <!-- End Title -->
       
-          <div class="max-w-3xl mx-auto mt-5 text-center">
-            <p class="text-lg text-gray-600 dark:text-neutral-400">Preline is a large open-source project, crafted with Tailwind CSS framework by Hmlstream.</p>
+          <div class="max-w-xl mx-auto mt-5 text-center">
+            <p class="text-lg text-gray-600 dark:text-neutral-400">Menyiapkan Generasi Pemimpin Masa Depan dengan Pendidikan Berkualitas</p>
           </div>
       
           <!-- Buttons -->
           <div class="flex justify-center gap-3 mt-8">
             <a class="inline-flex items-center justify-center px-4 py-3 text-sm font-medium text-center text-white border border-transparent rounded-full gap-x-3 bg-gradient-to-tl from-blue-600 to-violet-600 hover:from-violet-600 hover:to-blue-600 dark:focus:ring-offset-gray-800" href="#">
-              <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
-              </svg>
-              Continue with Github
+              <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32" fill="rgba(255,255,255,1)"><path fill="none" d="M0 0h24v24H0z"></path><path d="M18.031 16.6168L22.3137 20.8995L20.8995 22.3137L16.6168 18.031C15.0769 19.263 13.124 20 11 20C6.032 20 2 15.968 2 11C2 6.032 6.032 2 11 2C15.968 2 20 6.032 20 11C20 13.124 19.263 15.0769 18.031 16.6168ZM12.1779 7.17624C11.8055 7.06167 11.41 7 11 7C8.79086 7 7 8.79086 7 11C7 13.2091 8.79086 15 11 15C13.2091 15 15 13.2091 15 11C15 10.59 14.9383 10.1945 14.8238 9.82212C14.5102 10.5166 13.8115 11 13 11C11.8954 11 11 10.1046 11 9C11 8.18846 11.4834 7.48982 12.1779 7.17624Z"></path></svg>
+              Jelajahi Sekarang
             </a>
           </div>
           <!-- End Buttons -->
