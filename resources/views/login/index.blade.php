4bd74ffc696fe470bdd0c8f8f2d1a55a949bca23
Ilhan Haffiyan Juldan <ilhanzuldan11@gmail.com>
2024-06-10 17:31:45 +0700

    fix: register and login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,24 +1,30 @@
 @extends('layouts.main')
 
 @section('container')
-<div class="relative overflow-hidden before:absolute before:top-0 before:start-1/2 before:bg-glowRect before:bg-no-repeat before:bg-bottom before:size-full before:-z-[1] before:transform before:-translate-x-1/2">
-<div class="flex justify-center items-center min-h-screen">
+<div class="relative overflow-hidden before:absolute before:top-0 before:start-1/2 before:bg-glowRectBig before:bg-no-repeat before:bg-bottom before:size-full before:-z-[1] before:transform before:-translate-x-1/2">
+<div class="flex justify-center items-center min-h-screen p-2">
     <div class="w-full max-w-md p-6 border border-gray-500 dark:bg-[#030617] rounded-md shadow-md bg-white">
-        <div class="text-center">
-            <h2 class="text-2xl font-normal text-transparent sm:text-3xl bg-gradient-to-r bg-clip-text from-pink-600 via-purple-580 to-purple-700 inline-block">Welcome, TrailBlazer</h2>
-            <p class="mt-3 text-gray-500 text-justify dark:text-gray-500">Selamat datang di Portal Penerimaan Mahasiswa Universitas CheeseCool! Kelola aplikasi, unggah dokumen, dan alur pendaftaran yang mudah. Kami siap menyertai perjalanan pendidikan Anda!</p>
+        <div class="text-start py-5">
+            <h1 class="block text-2xl font-bold text-white md:text-3xl lg:text-4xl ">
+                <span class="font-bold  text-center lg:text-start text-transparent from-purple-600 via-pink-600 to-purple-600 bg-gradient-to-r bg-clip-text pt-20">                    Welcome, TrailBlazer
+                </span>
+                </h1>
+
+            <p class="mt-4 text-gray-500 dark:text-gray-400 text-justify">
+                Selamat datang di Portal Penerimaan Mahasiswa Universitas CheeseCool! Kelola aplikasi, unggah dokumen, dan alur pendaftaran yang mudah. Kami siap menyertai perjalanan pendidikan Anda!
+            </p>
         </div>
 
         <div class="mt-6">
             <form>
                 <div>
                     <label for="email" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email Address</label>
-                    <input type="email" name="email" id="email" placeholder="Email" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 dark:bg-[#030617] border border-gray-200 rounded-lg focus:outline-none focus:border-purple-500 focus:bg-white focus:ring focus:ring-purple-500 dark:text-gray-300 dark:border-gray-700 dark:focus:border-purple-500 dark:focus:bg-gray-900 dark:focus:ring-purple-500 dark:focus:ring-opacity-60" />
+                    <input type="email" name="email" id="email" placeholder="Email" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 dark:bg-[#030617] border border-gray-200 rounded-md focus:outline-none focus:border-purple-500 focus:bg-white focus:ring focus:ring-purple-500 dark:text-gray-300 dark:border-gray-700 dark:focus:border-purple-500 dark:focus:bg-gray-900 dark:focus:ring-purple-500 dark:focus:ring-opacity-60" />
                 </div>
 
                 <div class="mt-6">
                     <label for="password" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Password</label>
-                    <input type="password" name="password" id="password" placeholder="Password" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 dark:bg-[#030617] border border-gray-200 rounded-lg focus:outline-none focus:border-purple-500 focus:bg-white focus:ring focus:ring-purple-500 dark:text-gray-300 dark:border-gray-700 dark:focus:border-purple-500 dark:focus:bg-gray-900 dark:focus:ring-purple-500 dark:focus:ring-opacity-60" />
+                    <input type="password" name="password" id="password" placeholder="Password" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 dark:bg-[#030617] border border-gray-200 rounded-md focus:outline-none focus:border-purple-500 focus:bg-white focus:ring focus:ring-purple-500 dark:text-gray-300 dark:border-gray-700 dark:focus:border-purple-500 dark:focus:bg-gray-900 dark:focus:ring-purple-500 dark:focus:ring-opacity-60" />
                 </div>
 
                 <div class="mt-6">
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -2,28 +2,28 @@
 
 @section('container')
 
-<div class="relative overflow-hidden before:absolute before:-top-60 lg:before:top-0 before:start-1/2 before:bg-glowBottom before:bg-no-repeat  before:bg-bottom before:size-full before:-z-[1] before:transform before:-translate-x-1/2" >
+<div class="relative overflow-hidden before:absolute before:-top-60 lg:before:top-0 before:start-1/2 before:bg-glowRectBig before:bg-no-repeat before:bg-bottom before:size-full before:-z-[1] before:transform before:-translate-x-1/2 lg:py-10 pb-80" >
     <div class="flex justify-center min-h-screen">
         <div class="hidden bg-cover lg:block lg:w-2/5" style="background-image: url('/images/register-bg.png')">
         </div>
 
-        <div class="flex items-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5">
-            <div class="w-full p-6 border border-gray-500 dark:bg-oldNavy rounded-md shadow-md">
+        <div class="flex items-center w-full max-w-3xl p-2 mx-auto lg:px-8 lg:w-3/5">
+            <div class="w-full p-8  border border-gray-500 dark:bg-oldNavy rounded-md shadow-md">
                 <h1 class="block text-2xl font-bold text-white md:text-3xl lg:text-4xl ">
                     <span class="font-bold  text-center lg:text-start text-transparent from-purple-600 via-pink-600 to-purple-600 bg-gradient-to-r bg-clip-text pt-20">                    Jadilah Bagian dari Universitas Terbaik!
                     </span>
                     </h1>
 
-                <p class="mt-4 text-gray-500 dark:text-gray-400">
+                <p class="mt-4 text-gray-500 dark:text-gray-400 text-justify">
                      Daftarlah sekarang dan wujudkan impian Anda bersama Chesscool University ! Masa depan gemilang menanti Anda di sini
                 </p>
 
 
-                <form class="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2" action="/register" method="post">
+                <form class="flex flex-col flex-wrap gap-5 mt-8 py-5" action="/register" method="post">
                     @csrf
                     <div>
                         <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Name</label>
-                        <input type="text" placeholder="Name" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-oldNavy dark:text-gray-300 dark:border-gray-700 focus:border-teal-400 dark:focus:border-teal-400 focus:ring-teal-400 focus:outline-none focus:ring focus:ring-opacity-40 @error('name')is-invalid @enderror" name="name" required value="{{ old('name') }}"/>
+                        <input type="text" placeholder="Name" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-oldNavy dark:text-gray-300 dark:border-gray-700 focus:border-teal-400 dark:focus:border-teal-400 focus:ring-teal-400 focus:outline-none focus:ring focus:ring-opacity-40 @error('name')is-invalid @enderror" name="name" required value="{{ old('name') }}"/>
                         <label for="name">
                             @error('name')
                                 <p class="text-red-700">
@@ -35,7 +35,7 @@
 
                     <div>
                         <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Username</label>
-                        <input type="text" placeholder="Username" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-oldNavy dark:text-gray-300 dark:border-gray-700 focus:border-teal-400 dark:focus:border-teal-400 focus:ring-teal-400 focus:outline-none focus:ring focus:ring-opacity-40 @error('username')is-invalid @enderror" name="username"  required value="{{ old('username') }}"/>
+                        <input type="text" placeholder="Username" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-oldNavy dark:text-gray-300 dark:border-gray-700 focus:border-teal-400 dark:focus:border-teal-400 focus:ring-teal-400 focus:outline-none focus:ring focus:ring-opacity-40 @error('username')is-invalid @enderror" name="username"  required value="{{ old('username') }}"/>
                         <label for="username">
                             @error('username')
                                 <p class="text-red-700">
@@ -46,7 +46,7 @@
                     </div>
                     <div>
                         <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Email address</label>
-                        <input type="email" placeholder="Enter your email" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-oldNavy dark:text-gray-300 dark:border-gray-700 focus:border-teal-400 dark:focus:border-teal-400 focus:ring-teal-400 focus:outline-none focus:ring focus:ring-opacity-40 @error('email')is-invalid @enderror" name="email"  required value="{{ old('email') }}"/>
+                        <input type="email" placeholder="Enter your email" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-oldNavy dark:text-gray-300 dark:border-gray-700 focus:border-teal-400 dark:focus:border-teal-400 focus:ring-teal-400 focus:outline-none focus:ring focus:ring-opacity-40 @error('email')is-invalid @enderror" name="email"  required value="{{ old('email') }}"/>
                         <label for="email">
                             @error('email')
                                 <p class="text-red-700">
@@ -58,7 +58,7 @@
 
                     <div>
                         <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Password</label>
-                        <input type="password" placeholder="Enter your password" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-oldNavy dark:text-gray-300 dark:border-gray-700 focus:border-teal-400 dark:focus:border-teal-400 focus:ring-teal-400 focus:outline-none focus:ring focus:ring-opacity-40 @error('password')is-invalid @enderror" name="password" required value="{{ old('password') }}" />
+                        <input type="password" placeholder="Enter your password" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-oldNavy dark:text-gray-300 dark:border-gray-700 focus:border-teal-400 dark:focus:border-teal-400 focus:ring-teal-400 focus:outline-none focus:ring focus:ring-opacity-40 @error('password')is-invalid @enderror" name="password" required value="{{ old('password') }}" />
                         <label for="password">
                             @error('password')
                                 <p class="text-red-700">
@@ -73,6 +73,7 @@
                         <span>Register </span>
                     </button>
                 </form>
+                <p class="mt-6 text-sm text-center text-gray-400">Sudah Punya Akun ? <a href="/login" class="text-purple-500 focus:outline-none focus:underline hover:underline">Login</a>.</p>
             </div>
         </div>
     </div>
