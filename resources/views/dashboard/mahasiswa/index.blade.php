fe6ae39a08acdd70b586ee2e5719047d6c1c6772
Ilhan Haffiyan Juldan <ilhanzuldan11@gmail.com>
2024-06-14 10:52:07 +0700

    feat: delete and edit on calon mahasiswa table

--- resources/views/dashboard/mahasiswa/index.blade.php
+++ resources/views/dashboard/mahasiswa/index.blade.php
@@ -22,7 +22,7 @@
           </div>
         @endif
     </div>
-        <div class="-m-1.5 overflow-x-auto">
+        <div class="-m-1.5 overflow-y-auto">
           <div class="p-1.5 min-w-full inline-block align-middle">
           <!-- Table Section -->
         <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 mx-auto">
@@ -106,9 +106,9 @@
                   </div>
                 </th>
 
-                <th scope="col" class="px-6 py-3 text-start">
+                <th scope="col" class="px-6 py-3 text-start  border-e dark:border-neutral-700">
                   <div class="flex items-center gap-x-2">
-                    <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
+                    <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200 ">
                       Status
                     </span>
                   </div>
@@ -118,13 +118,6 @@
                   <div class="flex items-center gap-x-2">
                     <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
                       Action
-                    </span>
-                  </div>
-                </th>
-                <th scope="col" class="px-6 py-3 text-end">
-                  <div class="flex items-center gap-x-2">
-                    <span class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
-                      
                     </span>
                   </div>
                 </th>
@@ -173,7 +166,7 @@
                   </span>
                 
               </td>
-                <td class="size-px whitespace-nowrap">
+                <td class="size-px pe-4 whitespace-nowrap border-e dark:border-neutral-700">
                   @php
                       $status = $calonMahasiswa->nilai_test <= 80 ? 'tidak lulus' : 'lulus';
                   @endphp
@@ -192,31 +185,34 @@
                     </span> --}}
                   
                 {{-- </td>  --}}
-                <td class="size-px whitespace-nowrap cursor-pointer">
-                    <a href="/dashboard/jurusans/{{ $calonMahasiswa->id }}" class="">
+                <td class="size-px whitespace-nowrap">
+                  <div class="flex flex-col py-5  ">
+                    <div class="pb-2">
+                    <a href="/dashboard/mahasiswa/{{ $calonMahasiswa->id }}/edit" class="">
                     <span class="px-6 py-1.5">
-                      <span class="py-1 px-2 inline-flex justify-center items-center gap-2 rounded-lg border font-medium bg-white text-gray-700 shadow-sm align-middle hover: focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-yellow-600 transition-all text-sm dark:bg-yellow-500 dark:hover:bg-yellow-700 dark:border-neutral-700 dark:text-white dark:hover:text-white dark:focus:ring-offset-yellow-700">
+                      <span class="py-1 px-2 pr-7 inline-flex justify-center items-center gap-2 rounded-lg border font-medium bg-white text-gray-700 shadow-sm align-middle hover: focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-yellow-600 transition-all text-sm dark:bg-yellow-500 dark:hover:bg-yellow-700 dark:border-neutral-700 dark:text-white dark:hover:text-white dark:focus:ring-offset-yellow-700">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" fill="currentColor"><path fill="none" d="M0 0h24v24H0z"></path><path d="M5 18.89H6.41421L15.7279 9.57627L14.3137 8.16206L5 17.4758V18.89ZM21 20.89H3V16.6473L16.435 3.21231C16.8256 2.82179 17.4587 2.82179 17.8492 3.21231L20.6777 6.04074C21.0682 6.43126 21.0682 7.06443 20.6777 7.45495L9.24264 18.89H21V20.89ZM15.7279 6.74785L17.1421 8.16206L18.5563 6.74785L17.1421 5.33363L15.7279 6.74785Z"></path></svg>
-                      Edit
+                      Edit 
                     </span>
                   </span>
                 </a>
+              </div>
                   
-                </td>
-                <td class="size-px whitespace-nowrap cursor-pointer">
-                  <form action="/dashboard/jurusans/{{ $calonMahasiswa->id }}" action="post">
-                  @method('delete')
-                  @csrf
-                  <button class="" onclick="return confirm('Are you sure ?')">
+               
+                  <form action="/dashboard/mahasiswa/{{ $calonMahasiswa->id }}" method="POST">
+                    @method('delete')
+                    @csrf
+                  <button class="" onclick="return confirm('Yakin menghapus data ?')">
                     <span class="px-6 py-1.5">
                       <span class="py-1 px-2 inline-flex justify-center items-center gap-2 rounded-lg border font-medium bg-white text-gray-700 shadow-sm align-middle hover: focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-red-600 transition-all text-sm dark:bg-red-500 dark:hover:bg-red-700 dark:border-neutral-700 dark:text-white dark:hover:text-white dark:focus:ring-offset-red-800">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" fill="currentColor"><path fill="none" d="M0 0h24v24H0z"></path><path d="M17 6H22V8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8H2V6H7V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V6ZM18 8H6V20H18V8ZM9 11H11V17H9V11ZM13 11H15V17H13V11ZM9 4V6H15V4H9Z"></path></svg>
-                      Delete
+                      Hapus
                     </span>
                   </span>
                 </button>
                   </form>
                   
+                </div>
                 </td>
               </tr>
               @endforeach
